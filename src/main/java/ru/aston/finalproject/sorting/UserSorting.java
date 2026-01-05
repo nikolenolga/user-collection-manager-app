e6f70b6d8a949733550454bad046fc5870f24bde
Lich <lonedarkwolf.09@gmail.com>
2026-01-05 13:14:30 +0300

    Добавил сортировку

---
+++ src/main/java/ru/aston/finalproject/sorting/UserSorting.java
@@ -0,0 +1,41 @@
+package ru.aston.finalproject.sorting;
+
+import ru.aston.finalproject.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserSorting {
+    public static void mergeSort (List<User> list) {
+        if (list.size() <= 1) return;
+
+        int mid = list.size() / 2;
+        List<User> left = new ArrayList<>(list.subList(0, mid));
+        List<User> right = new ArrayList<>(list.subList(mid, list.size()));
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(list, left, right);
+    }
+
+    private static void merge(List<User> result, List<User> left, List<User> right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < left.size() && j < right.size()) {
+            if (left.get(i).compareTo(right.get(j)) <= 0) {
+                result.set(k++, left.get(i++));
+            } else {
+                result.set(k++, right.get(j++));
+            }
+        }
+        while (i < left.size()) {
+            result.set(k++, left.get(i++));
+        }
+        while (j < right.size()) {
+            result.set(k++, right.get(j++));
+        }
+    }
+}
